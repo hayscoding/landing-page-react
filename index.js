48507affa7119cdbb2fe12db4ba5a397ddcd3f82
Hays Stanford <stanfordhays@gmail.com>
2019-11-16 15:45:34 -0600

    init create-react-library@2.6.7

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-import Button from './lib/components/Button.js';
-
-export { default as Button } from './lib/components/Button.js';
+export { default as Button } from './lib/components/Button/Button.js';
 
 export const printMsg = function() {
 	console.log('Here is our test message from our NPM package.')
