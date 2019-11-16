bd97f97737a04dad0e74afa75e9d692c90db9654
Hays Stanford <stanfordhays@gmail.com>
2019-11-16 14:07:32 -0600

    commented out code in Button.js for testing

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-exports.printMsg = function() {
+import Button from './lib/components/Button.js';
+
+export { default as Button } from './lib/components/Button.js';
+
+export const printMsg = function() {
 	console.log('Here is our test message from our NPM package.')
 }
